51d2764ad85b54e50144174dbcfae71434a84b02
Andricsak <alex.andricsak@gmail.com>
2018-07-02 18:12:03 +0300

    Timer code

---
+++ Timer.c
@@ -0,0 +1,16 @@
+struct timer
+{
+	chrono::time_point<chrono::steady_clock> start, end;
+	chrono::duration<float> duration;
+	timer()
+	{
+		 start = chrono::high_resolution_clock::now();
+	}
+	~timer()
+	{
+		end = chrono::high_resolution_clock::now();
+		duration = end - start;
+		float ms = duration.count()*1000.0f;
+		cout << "Timer:" << ms << " ms\n";
+	}
+};
